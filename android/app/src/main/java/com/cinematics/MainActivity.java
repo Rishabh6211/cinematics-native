542234272649da40bec174a3eb0356728b4f9f4b
Rishabh Gupta <rishabh.gupta@Mac-Mini-3.local>
2018-03-12 12:52:06 +0530

    splash screen android

--- android/app/src/main/java/com/cinematics/MainActivity.java
+++ android/app/src/main/java/com/cinematics/MainActivity.java
@@ -1,7 +1,8 @@
 package com.cinematics;
 
 import com.facebook.react.ReactActivity;
-
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 public class MainActivity extends ReactActivity {
 
     /**
@@ -11,5 +12,11 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "cinematics";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
     }
 }
