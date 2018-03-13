cda3ec69aa2c63c6afc1619e5de8b3ae2fb94624
Rishabh Gupta <rishabh.gupta@Mac-Mini-3.local>
2018-03-13 10:35:05 +0530

    scrollable tab

--- src/component/scrolltab.js
+++ src/component/scrolltab.js
@@ -1,20 +1,27 @@
-import  ScrollableTabView from 'react-native-scrollable-tab-view';
+import ScrollableTabView, { ScrollableTabBar, } from 'react-native-scrollable-tab-view';
 import React, { Component } from 'react';
 import Home1 from './home1'
 import Home2 from './home2'
+
  export default class Tab extends Component {
 
   render() {
     
     return (
 
-    <ScrollableTabView>
-        <Home1 tabLabel="React" />
-        <Home2 tabLabel="Flow" />
-        <Home2 tabLabel="Flow" />
-        <Home2 tabLabel="Flow" />
-        <Home2 tabLabel="Flow" />
-        <Home2 tabLabel="Flow" />
+    <ScrollableTabView tabBarBackgroundColor="#1b1b21" 
+        tabBarActiveTextColor="#FFFFFF" 
+        tabBarTextStyle={{fontFamily: 'Roboto', fontSize: 17, color:"white"}}
+        tabBarUnderlineStyle={{backgroundColor:"green"}}
+        initialPage={0}
+        renderTabBar={() => <ScrollableTabBar />}
+    >
+        <Home1 tabLabel="NOW PLAYING" />
+        <Home2 tabLabel="TOP BOX OFFICE" />
+        <Home2 tabLabel="ANTICIPATED" />
+        <Home2 tabLabel="UPCOMING" />
+        <Home2 tabLabel="TRENDING" />
+        <Home2 tabLabel="TOP RATED" />
     </ScrollableTabView>
 
     );
